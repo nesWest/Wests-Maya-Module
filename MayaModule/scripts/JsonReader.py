bee12ed30f7c3ad17fd58f908a7199ff200eb711
West Foulks <westfoulks@gmail.com>
2024-07-23 17:34:44 -0700

    Adding more parser functions

--- MayaModule/scripts/JsonReader.py
+++ MayaModule/scripts/JsonReader.py
@@ -3,13 +3,20 @@ import os
 
 dataName = 'module-data.json'
 
-def GetShelves() -> dict:
+def GetPath() -> str:
     path = os.path.dirname(__file__).replace("MayaModule\\scripts","") 
     path += dataName
+    return path
+
+def GetData() -> dict:
+    path = GetPath()
 
     with open(path, 'r') as file:
         data = json.load(file)
         file.close()
 
-    return data['shelves']
+    return data
+
+def GetShelves() -> dict:
+    return GetData()['shelves']
 
